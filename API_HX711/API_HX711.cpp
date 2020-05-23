f637be9482e3cd29502f44a442a31848360b3a0a
huzeyfepiroglu <a.huzeyfepiroglu@gmail.com>
2020-05-23 14:55:16 +0300

    Files formatted
    Files formatted

--- API_HX711/API_HX711.cpp
+++ API_HX711/API_HX711.cpp
@@ -19,34 +19,34 @@
 #include <API_HX711.h>
 #include "API_Delay_uS.h"
 
+uint8_t shiftInSlow(GPIO_TypeDef *dataPin_port, uint8_t dataPin, GPIO_TypeDef *clockPin_port, uint8_t clockPin, uint8_t bitOrder)
+{
+	uint8_t value = 0;
+	uint8_t i;
 
-
-uint8_t shiftInSlow(GPIO_TypeDef* dataPin_port,uint8_t dataPin,GPIO_TypeDef* clockPin_port, uint8_t clockPin, uint8_t bitOrder) {
-    uint8_t value = 0;
-    uint8_t i;
-
-    for(i = 0; i < 8; ++i) {
-			  HAL_GPIO_WritePin(clockPin_port,clockPin,GPIO_PIN_SET);
-        Delay_uS(1);
-        if(bitOrder == LSBFIRST)
-            value |= HAL_GPIO_ReadPin(dataPin_port,dataPin)<< i;
-        else
-            value |= HAL_GPIO_ReadPin(dataPin_port,dataPin) << (7 - i);
-        HAL_GPIO_WritePin(clockPin_port,clockPin,GPIO_PIN_RESET);
-        Delay_uS(1);
-    }
-    return value;
+	for (i = 0; i < 8; ++i)
+	{
+		HAL_GPIO_WritePin(clockPin_port, clockPin, GPIO_PIN_SET);
+		Delay_uS(1);
+		if (bitOrder == LSBFIRST)
+			value |= HAL_GPIO_ReadPin(dataPin_port, dataPin) << i;
+		else
+			value |= HAL_GPIO_ReadPin(dataPin_port, dataPin) << (7 - i);
+		HAL_GPIO_WritePin(clockPin_port, clockPin, GPIO_PIN_RESET);
+		Delay_uS(1);
+	}
+	return value;
 }
 
-#define SHIFTIN_WITH_SPEED_SUPPORT(dataPin_port,dataPin,clockPin_port,clockPin,order) shiftInSlow(dataPin_port,dataPin,clockPin_port,clockPin,order)
+#define SHIFTIN_WITH_SPEED_SUPPORT(dataPin_port, dataPin, clockPin_port, clockPin, order) shiftInSlow(dataPin_port, dataPin, clockPin_port, clockPin, order)
 
-HX711::HX711() {
+HX711::HX711()
+{
 }
 
-HX711::~HX711() {
+HX711::~HX711()
+{
 }
-
-
 
 /*
 void HX711::begin(int dout, int pd_sck, int gain) {
@@ -59,44 +59,49 @@ void HX711::begin(int dout, int pd_sck, int gain) {
 	set_gain(gain);
 }
 
-Bu kisimda pin tanimlamasi yapiliyor. Zaten Cubemxte pinleri tanimladigimiz için
+Bu kisimda pin tanimlamasi yapiliyor. Zaten Cubemxte pinleri tanimladigimiz iï¿½in
 PD_SCK 'yi digital output ve DOUT 'u ise digital input olarak tanimlamak yetecektir.
-set_gain fonksiyomnu ise basta çagirilmali. bu yüzden fonksiyon bu sekilde degistirildi.
+set_gain fonksiyomnu ise basta cagirilmali. bu yuzden fonksiyon bu sekilde degistirildi.
 */
-void HX711::begin(uint8_t gain) {
+void HX711::begin(uint8_t gain)
+{
 
 	set_gain(gain);
 }
 
-bool HX711::is_ready() {
-	return HAL_GPIO_ReadPin(DOUT_GPIO_Port,DOUT_Pin) == false;	
+bool HX711::is_ready()
+{
+	return HAL_GPIO_ReadPin(DOUT_GPIO_Port, DOUT_Pin) == false;
 }
 
-void HX711::set_gain(uint8_t gain) {
-	switch (gain) {
-		case 128:		// channel A, gain factor 128
-			GAIN = 1;
-			break;
-		case 64:		// channel A, gain factor 64
-			GAIN = 3;
-			break;
-		case 32:		// channel B, gain factor 32
-			GAIN = 2;
-			break;
+void HX711::set_gain(uint8_t gain)
+{
+	switch (gain)
+	{
+	case 128: // channel A, gain factor 128
+		GAIN = 1;
+		break;
+	case 64: // channel A, gain factor 64
+		GAIN = 3;
+		break;
+	case 32: // channel B, gain factor 32
+		GAIN = 2;
+		break;
 	}
-	HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_RESET);
 
 	read();
 }
 
-long HX711::read() {
+long HX711::read()
+{
 
 	// Wait for the chip to become ready.
 	wait_ready();
 
 	// Define structures for reading data into.
 	unsigned long value = 0;
-	uint8_t data[3] = { 0 };
+	uint8_t data[3] = {0};
 	uint8_t filler = 0x00;
 
 	// Protect the read sequence from system interrupts.  If an interrupt occurs during
@@ -110,56 +115,61 @@ long HX711::read() {
 	__disable_irq();
 
 	// Pulse the clock pin 24 times to read the data.
-	data[2] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port,DOUT_Pin,PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
-	data[1] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port,DOUT_Pin,PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
-	data[0] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port,DOUT_Pin,PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
+	data[2] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port, DOUT_Pin, PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
+	data[1] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port, DOUT_Pin, PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
+	data[0] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT_GPIO_Port, DOUT_Pin, PD_SCK_GPIO_Port, PD_SCK_Pin, MSBFIRST);
 
 	// Set the channel and the gain factor for the next reading using the clock pin.
-	for (uint8_t i = 0; i < GAIN; i++) {
-		HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_SET);
+	for (uint8_t i = 0; i < GAIN; i++)
+	{
+		HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_SET);
 		Delay_uS(1);
-		HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_RESET);
 		Delay_uS(1);
 	}
 
 	// Enable interrupts again.
 	__enable_irq();
 
-
 	// Replicate the most significant bit to pad out a 32-bit signed integer
-	if (data[2] & 0x80) {
+	if (data[2] & 0x80)
+	{
 		filler = 0xFF;
-	} else {
+	}
+	else
+	{
 		filler = 0x00;
 	}
 
 	// Construct a 32-bit signed integer
-	value = ( static_cast<unsigned long>(filler)  << 24
-				  | static_cast<unsigned long>(data[2]) << 16
-				  | static_cast<unsigned long>(data[1]) << 8
-				  | static_cast<unsigned long>(data[0]) );
+	value = (static_cast<unsigned long>(filler) << 24 | static_cast<unsigned long>(data[2]) << 16 | static_cast<unsigned long>(data[1]) << 8 | static_cast<unsigned long>(data[0]));
 
 	return static_cast<long>(value);
 }
 
-void HX711::wait_ready(unsigned long delay_ms) {
+void HX711::wait_ready(unsigned long delay_ms)
+{
 	// Wait for the chip to become ready.
 	// This is a blocking implementation and will
 	// halt the sketch until a load cell is connected.
-	while (!is_ready()) {
+	while (!is_ready())
+	{
 		// Probably will do no harm on AVR but will feed the Watchdog Timer (WDT) on ESP.
 		// https://github.com/bogde/HX711/issues/73
 		HAL_Delay(delay_ms);
 	}
 }
 
-bool HX711::wait_ready_retry(uint8_t retries, unsigned long delay_ms) {
+bool HX711::wait_ready_retry(uint8_t retries, unsigned long delay_ms)
+{
 	// Wait for the chip to become ready by
 	// retrying for a specified amount of attempts.
 	// https://github.com/bogde/HX711/issues/76
 	int count = 0;
-	while (count < retries) {
-		if (is_ready()) {
+	while (count < retries)
+	{
+		if (is_ready())
+		{
 			return true;
 		}
 		HAL_Delay(delay_ms);
@@ -168,12 +178,15 @@ bool HX711::wait_ready_retry(uint8_t retries, unsigned long delay_ms) {
 	return false;
 }
 
-bool HX711::wait_ready_timeout(unsigned long timeout, unsigned long delay_ms) {
+bool HX711::wait_ready_timeout(unsigned long timeout, unsigned long delay_ms)
+{
 	// Wait for the chip to become ready until timeout.
 	// https://github.com/bogde/HX711/pull/96
 	unsigned long millisStarted = millis();
-	while (millis() - millisStarted < timeout) {
-		if (is_ready()) {
+	while (millis() - millisStarted < timeout)
+	{
+		if (is_ready())
+		{
 			return true;
 		}
 		HAL_Delay(delay_ms);
@@ -181,9 +194,11 @@ bool HX711::wait_ready_timeout(unsigned long timeout, unsigned long delay_ms) {
 	return false;
 }
 
-long HX711::read_average(uint8_t times) {
+long HX711::read_average(uint8_t times)
+{
 	long sum = 0;
-	for (int i = 0; i < times; i++) {
+	for (int i = 0; i < times; i++)
+	{
 		sum += read();
 		// Probably will do no harm on AVR but will feed the Watchdog Timer (WDT) on ESP.
 		// https://github.com/bogde/HX711/issues/73
@@ -192,40 +207,49 @@ long HX711::read_average(uint8_t times) {
 	return sum / times;
 }
 
-double HX711::get_value(uint8_t times) {
+double HX711::get_value(uint8_t times)
+{
 	return read_average(times) - OFFSET;
 }
 
-float HX711::get_units(uint8_t times) {
+float HX711::get_units(uint8_t times)
+{
 	return get_value(times) / SCALE;
 }
 
-void HX711::tare(uint8_t times) {
+void HX711::tare(uint8_t times)
+{
 	double sum = read_average(times);
 	set_offset(sum);
 }
 
-void HX711::set_scale(float scale) {
+void HX711::set_scale(float scale)
+{
 	SCALE = scale;
 }
 
-float HX711::get_scale() {
+float HX711::get_scale()
+{
 	return SCALE;
 }
 
-void HX711::set_offset(long offset) {
+void HX711::set_offset(long offset)
+{
 	OFFSET = offset;
 }
 
-long HX711::get_offset() {
+long HX711::get_offset()
+{
 	return OFFSET;
 }
 
-void HX711::power_down() {
-  HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_SET);
+void HX711::power_down()
+{
+	HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_SET);
 }
 
-void HX711::power_up() {
-	HAL_GPIO_WritePin(PD_SCK_GPIO_Port,PD_SCK_Pin,GPIO_PIN_RESET);
+void HX711::power_up()
+{
+	HAL_GPIO_WritePin(PD_SCK_GPIO_Port, PD_SCK_Pin, GPIO_PIN_RESET);
 }
